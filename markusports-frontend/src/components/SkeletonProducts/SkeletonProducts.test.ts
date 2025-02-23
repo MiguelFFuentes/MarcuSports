1f63839c6063e8cd998c3a9a64b857a033ce8e0e
Miguel <miguel.fernandez@minteando.me>
2025-02-23 18:31:47 +0100

    refactor: rename skeletonproducts component

--- markusports-frontend/src/components/SkeletonProducts/SkeletonProducts.test.ts
+++ markusports-frontend/src/components/SkeletonProducts/SkeletonProducts.test.ts
@@ -1,9 +1,9 @@
 import { mount } from '@vue/test-utils'
 import { describe, it, expect } from 'vitest'
-import LoadingProducts from '@/components/LoadingProducts/LoadingProducts.vue'
+import LoadingProducts from '@/components/SkeletonProducts/SkeletonProducts.vue'
 import vuetify from "@/plugins/vuetify";
 
-describe('LoadingProducts.vue', () => {
+describe('SkeletonProducts.vue', () => {
   it('should render the correct number of skeleton loaders based on productsCount prop', () => {
     const productsCount = 5
     const wrapper = mount(LoadingProducts, {
