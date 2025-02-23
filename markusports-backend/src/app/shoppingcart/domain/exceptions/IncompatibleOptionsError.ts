854369e299c3bc3c58cdb2d14239b84aa9a6b924
Miguel <miguel.fernandez@minteando.me>
2025-02-23 18:37:27 +0100

    feat: add optionId to IncompatibleOptionsError

--- markusports-backend/src/app/shoppingcart/domain/exceptions/IncompatibleOptionsError.ts
+++ markusports-backend/src/app/shoppingcart/domain/exceptions/IncompatibleOptionsError.ts
@@ -1,5 +1,5 @@
 export class IncompatibleOptionsError extends Error {
-    constructor(productId: number, optionId: number) {
+    constructor(productId: number, public optionId: number) {
         super(`Option ${optionId} from Product ${productId} is not compatible`)
     }
 }
